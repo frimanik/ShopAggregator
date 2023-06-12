dc1735aa957c7cfc6125c542ced82428c0d89de1
frimanik <frimanik@gmail.com>
2023-06-12 19:54:46 +0500

    Finished customizing query for possibility for  several requests.

--- src/main/java/com/Nullton/ShopAggregator/AmazonParser/AmazonParser.java
+++ src/main/java/com/Nullton/ShopAggregator/AmazonParser/AmazonParser.java
@@ -11,37 +11,39 @@ import java.util.List;
 public class AmazonParser implements DataFetcher {
     @Override
     public List<ProductEntity> Fetch(String product, int quantity) throws IOException {
-        String URL = "https://amazon.com/s?k="+product+"&s=price-asc-rank";
-        AmazonHttpConnection connection = new AmazonHttpConnection();
+        int page = 1;
         HashMap<String, ProductEntity> products = new HashMap<>();
+        while (products.size() < quantity) {
+            String URL = "https://amazon.com/s?k=" + product + "&s=price-asc-rank&page=" + page++;
+            AmazonHttpConnection connection = new AmazonHttpConnection();
+            String content = connection.configureConnection(URL);
+            int id1 = content.indexOf("img class=\"s-image\"");
+            int id2 = content.lastIndexOf("img class=\"s-image\"");
+            String script = content.substring(id1, id2);
+            String[] split = script.split("<span class=\"a-offscreen\">|<span class=\"a-size-medium a-color-base a-text-normal\">|</span>|img class=\"s-image\" src=\"|srcset=");
+            String temp = "";
 
-        String content = connection.configureConnection(URL);
-        int id1 = content.indexOf("img class=\"s-image\"");
-        int id2 = content.lastIndexOf("img class=\"s-image\"");
-        String script = content.substring(id1, id2);
-        String[] split = script.split("<span class=\"a-offscreen\">|<span class=\"a-size-medium a-color-base a-text-normal\">|</span>|img class=\"s-image\" src=\"|srcset=");
-        String temp = "";
-
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].endsWith("jpg\" ")) {
-                split[i] = split[i].substring(0, split[i].length() - 2);
-                products.put(split[i], new AmazonProductEntity());
-                temp = split[i];
-                products.get(temp).setImgUrl(temp);
-                products.get(temp).setId();
-                products.get(temp).setShop("Amazon.com");
-            }
-
-            if (split[i].contains("<span class=\"a-size-base-plus a-color-base a-text-normal\">"))
-                try {
-                    products.get(temp).setTitle(split[i].substring(split[i].indexOf("a-text-normal\">") + 15, split[i].length() - 1));
-                } catch (NullPointerException e) {
-                    e.getMessage();
+            for (int i = 0; i < split.length; i++) {
+                if (split[i].endsWith("jpg\" ")) {
+                    split[i] = split[i].substring(0, split[i].length() - 2);
+                    products.put(split[i], new AmazonProductEntity());
+                    temp = split[i];
+                    products.get(temp).setImgUrl(temp);
+                    products.get(temp).setId();
+                    products.get(temp).setShop("Amazon.com");
                 }
-            if (split[i].startsWith("$")) {
-                products.get(temp).setPrice(BigDecimal.valueOf(Double.parseDouble(split[i].substring(1, split[i].length() - 1))));
+
+                if (split[i].contains("<span class=\"a-size-base-plus a-color-base a-text-normal\">"))
+                    try {
+                        products.get(temp).setTitle(split[i].substring(split[i].indexOf("a-text-normal\">") + 15, split[i].length() - 1));
+                    } catch (NullPointerException e) {
+                        e.getMessage();
+                    }
+                if (split[i].startsWith("$")) {
+                    products.get(temp).setPrice(BigDecimal.valueOf(Double.parseDouble(split[i].substring(1, split[i].length() - 1))));
+                }
             }
         }
-        return products.values().stream().filter(productEntity -> productEntity.price!=null).toList();
+        return products.values().stream().filter(productEntity -> productEntity.price != null).toList();
     }
 }
