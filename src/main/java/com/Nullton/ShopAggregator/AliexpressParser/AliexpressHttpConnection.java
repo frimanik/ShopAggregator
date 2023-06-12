de00a9a62d30568dda8bf0cddc07c0fd93c6308e
frimanik <frimanik@gmail.com>
2023-06-12 17:25:11 +0500

    Architecture and logic revision.

--- src/main/java/com/Nullton/ShopAggregator/AliexpressParser/AliexpressHttpConnection.java
+++ src/main/java/com/Nullton/ShopAggregator/AliexpressParser/AliexpressHttpConnection.java
@@ -1,18 +1,14 @@
 package com.Nullton.ShopAggregator.AliexpressParser;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.Nullton.ShopAggregator.HttpConnection;
 import com.gargoylesoftware.htmlunit.WebClient;
 
 import java.io.IOException;
 
-public class AliexpressHttpConnection {
+public class AliexpressHttpConnection  extends HttpConnection {
 
-    public String fetchData(String URL,String product) throws IOException {
-        WebClient webClient = new WebClient(BrowserVersion.FIREFOX);
-        webClient.getOptions().setJavaScriptEnabled(true);
-        webClient.getOptions().setThrowExceptionOnScriptError(false);
-        String content = webClient.getPage(URL+"/w/wholesale-"+product+".html?&sortType=price_asc").getWebResponse().getContentAsString();
-        webClient.close();
-        return content;
+    @Override
+    public String LoadContent(String URL) throws IOException {
+        return super.configureConnection(URL);
     }
 }
