705aa98abe856f1e32b7ea950816aa4cafa8b806
frimanik <frimanik@gmail.com>
2023-06-12 19:57:15 +0500

    Fixed query for several requests.

--- src/main/java/com/Nullton/ShopAggregator/AliexpressParser/AliexpressParser.java
+++ src/main/java/com/Nullton/ShopAggregator/AliexpressParser/AliexpressParser.java
@@ -12,12 +12,11 @@ public class AliexpressParser implements DataFetcher {
 
     @Override
     public List<ProductEntity> Fetch(String product, int quantity) throws IOException {
-        String URL = "https://www.aliexpress.com/w/wholesale-"+product+".html?&sortType=price_asc";
         AliexpressHttpConnection connection = new AliexpressHttpConnection();
         HashMap<String, ProductEntity> products = new HashMap<>();
-        int page = 0;
+        int page = 1;
         while (products.size() < quantity) {
-            page++;
+            String URL = "https://www.aliexpress.com/w/wholesale-"+product+".html?&sortType=price_asc&page="+page++;
             String content = connection.configureConnection(URL);
             String script = content.substring(content.indexOf("dida_config"), content.lastIndexOf("dida_config"));
             String[] split = script.split("[,:\"]");
@@ -37,9 +36,7 @@ public class AliexpressParser implements DataFetcher {
                     products.get(temp).setPrice(BigDecimal.valueOf(Double.parseDouble(split[i + 2])));
                 }
             }
-            URL = URL.replace("page=" + (page - 1), "page=" + page);
         }
-
         return products.values().stream().filter(productEntity -> productEntity.price!=null).toList();
     }
 }
