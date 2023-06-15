86ff43956275f4eacc14d59633e930314c6cfc5c
frimanik <frimanik@gmail.com>
2023-06-15 09:29:28 +0500

    temp test

--- src/main/java/com/Nullton/ShopAggregator/ShopAggregatorApplication.java
+++ src/main/java/com/Nullton/ShopAggregator/ShopAggregatorApplication.java
@@ -1,6 +1,7 @@
 package com.Nullton.ShopAggregator;
 
 import com.Nullton.ShopAggregator.AliexpressParser.AliexpressParser;
+import com.Nullton.ShopAggregator.AmazonParser.AmazonParser;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -11,9 +12,13 @@ public class ShopAggregatorApplication {
 
     public static void main(String[] args) throws IOException {
 //		SpringApplication.run(ShopAggregatorApplication.class, args);
+        HttpConnection.openConnection();
+        CurrencyExchangeRateProvider.computeCurrencyToDollar("PHP");
         ShopProvider provider = new ShopProvider(new LinkedList<>());
         provider.Add(new AliexpressParser());
+        provider.Add(new AmazonParser());
         ShopAggregator shopAggregator = new ShopAggregator(provider);
-        shopAggregator.searchCheapest("fridge",1);
+        shopAggregator.searchCheapest("lamp",1);
+        HttpConnection.closeConnection();
     }
 }
